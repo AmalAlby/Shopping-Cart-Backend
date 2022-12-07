17894d384e88a2d469662f254be3375c627d0123
AmalAlby <amalalbyn@gmail.com>
2022-12-07 20:25:05 +0530

    Shopping cart product dao created

--- src/main/java/com/example/shoppingcartnewbackend/controller/ShoppingCartController.java
+++ src/main/java/com/example/shoppingcartnewbackend/controller/ShoppingCartController.java
@@ -1,28 +1,41 @@
 package com.example.shoppingcartnewbackend.controller;
 
+import com.example.shoppingcartnewbackend.dao.ProductDao;
 import com.example.shoppingcartnewbackend.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ShoppingCartController {
 
+    @Autowired
+    private ProductDao dao;
+
     @CrossOrigin(origins = "*")
     @PostMapping(path="/addp",consumes ="application/json",produces ="application/json")
-    public String addproduct(@RequestBody Product p){
-        System.out.println(p.getName().toString());
-        return "Add product successfull";
+    public Map<String,String> addproduct(@RequestBody Product p){
+        dao.save(p);
+        HashMap<String,String> map=new HashMap<>();
+        map.put("status","success");
+        return map ;
     }
 
     @CrossOrigin(origins ="*")
     @GetMapping("/viewp")
-    public String viewproduct(){
-        return "View successfull";
+    public List<Product> viewproduct(){
+        return (List<Product>) dao.findAll();
     }
 
     @CrossOrigin(origins = "*")
-    @PostMapping("/searchp")
-    public String searchproduct(){
-        return "Search successfull";
+    @PostMapping(path="/searchp",consumes = "application/json",produces = "application/json")
+    public List<Product> searchproduct(@RequestBody Product p){
+        String pname=p.getName().toString();
+        System.out.println(pname);
+        return (List<Product>) dao.SearchProduct(p.getName()) ;
     }
 
 }
