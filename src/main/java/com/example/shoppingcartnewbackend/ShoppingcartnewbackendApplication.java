60a8e45b1ad5e34399b2cb81df690934a7508313
AmalAlby <amalalbyn@gmail.com>
2022-12-07 18:05:30 +0530

    Shopping cart backend created

---
+++ src/main/java/com/example/shoppingcartnewbackend/ShoppingcartnewbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoppingcartnewbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingcartnewbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingcartnewbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shoppingcartnewbackend/ShoppingcartnewbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppingcartnewbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingcartnewbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
